a54944731e46de9ca803a3f9f336a318818d828b
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2023-11-29 13:07:58 -0600

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "doings"
+#    storage_account_name = "doings"
+#    container_name       = "doings"
+#    key                  = "terraform.tfstate"
+#  }
+#}
